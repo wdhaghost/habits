1ed0573f5d49b68ec6a02ccd066c35fc4032d7b5
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-02 18:08:13 +0100

    add model to retreive data from component

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -1,11 +1,11 @@
 <script setup>
 import {ref} from 'vue';
 const props = defineProps(['name','type'])
-const value= ref('');
+const model= defineModel()
 
 </script>
 <template>
     <div class="my-2 p-4 w-full">
-        <input class="bg-gray-100 p-4 rounded-xl w-full" :type="type" v-model="value" :placeholder="name">
+        <input class="bg-gray-100 p-4 rounded-xl w-full" :type="type" v-model="model" :placeholder="name">
     </div>
 </template>
