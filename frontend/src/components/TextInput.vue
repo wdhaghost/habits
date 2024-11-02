5f08661c3338e31c1ca3ac6493922a7c74e9ad84
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-02 16:06:11 +0100

    Textinput created

---
+++ frontend/src/components/TextInput.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from 'vue';
+const props = defineProps(['name','type'])
+const value= ref('');
+
+</script>
+<template>
+    <div class="my-2 w-25">
+        <input class="bg-gray-100 p-4 rounded-xl w-full" :type="type" v-model="value" :placeholder="name">
+    </div>
+</template>
