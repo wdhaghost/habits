a5e62ea84af6faae57206671f4132ceb20dae510
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-02 16:42:06 +0100

    styling w/ tailwind signup view

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,15 +1,17 @@
 <template lang="">
-    <div class="m-4 dvw-full h-4/5 bg-red-500">
-        <div class="flex flex-col items-center w-50 ">
-            <TextInput name="name" type="text"/>
-            <TextInput name="email" type="text"/>
-            <TextInput name="password" type="password"/>
-            <button>Incription</button>
+    <div class="flex flex-col items-center justify-center">
+        <p class="font-bold text-xl">Inscription</p>
+        <div class="w-full  md:w-1/2 h-4/5 flex flex-col items-center w-50 ">
+            <TextInput name="Name" type="text"/>
+            <TextInput name="Email" type="text"/>
+            <TextInput name="Password" type="password"/>
+            <button class="bg-blue-500 font-bold text-center text-white w-[350px] py-4 rounded-full">Incription</button>
         </div>
     </div>
 </template>
 <script setup>
 import TextInput from '@/components/TextInput.vue';
+
 
 </script>
 <style lang="">
