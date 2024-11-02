4aa1c704a10bab9a32ad42647f6fe8d023b45002
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-02 16:06:29 +0100

    signup view created

---
+++ frontend/src/views/SignUpView.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div class="m-4 dvw-full h-4/5 bg-red-500">
+        <div class="flex flex-col items-center w-50 ">
+            <TextInput name="name" type="text"/>
+            <TextInput name="email" type="text"/>
+            <TextInput name="password" type="password"/>
+            <button>Incription</button>
+        </div>
+    </div>
+</template>
+<script setup>
+import TextInput from '@/components/TextInput.vue';
+
+</script>
+<style lang="">
+    
+</style>
